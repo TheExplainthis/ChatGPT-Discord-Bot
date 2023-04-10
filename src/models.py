43581d78ee7146360c8fe2b450a7a684eeaa32c0
Aryan Donga <daryan6680@proton.me>
2023-04-10 19:40:41 +0530

    Update models.py

--- src/models.py
+++ src/models.py
@@ -17,7 +17,7 @@ class OpenAIModel(ModelInterface):
         self.image_size = image_size
 
     def chat_completion(self, messages) -> str:
-        return openai.Completion.create(
+        return openai.ChatCompletion.create(
             model=self.model_engine, messages=messages
         )
 
